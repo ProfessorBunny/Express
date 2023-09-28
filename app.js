1906ff0e3dd55225516a6ede5dc2d21ac3d3c4dd
200~ProfessorBunny <nikunjraghav24@gmail.com>
2023-09-28 21:09:35 +0100

    handling multi url request

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'content-type': 'text/html' })
-    res.write('<h1> home page </h1> ')
-    res.end()
+    if (url === '/') {
+        res.writeHead(200, { 'content-type': 'text/html' })
+        res.write(homePage)
+        res.end()
+    }
+    // about page
+    else if (url === '/about') {
+        res.writeHead(200, { 'content-type': 'text/html' })
+        res.write('<h1>about page</h1>')
+        res.end()
+    }
+    else {
+        res.writeHead(404, { 'content-type': 'text/html' })
+        res.write('<h1>about page</h1>')
+        res.end()
+    }
 })
 
 server.listen(5000)
