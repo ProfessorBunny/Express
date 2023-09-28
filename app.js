a885e18d0025e79de479352fcf3441a5ffa8b55b
200~ProfessorBunny <nikunjraghav24@gmail.com>
2023-09-28 21:11:22 +0100

    bug fix

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    if (url === '/') {
+    if (req.url === '/') {
         res.writeHead(200, { 'content-type': 'text/html' })
-        res.write(homePage)
+        res.write('<h1>Home page</h1>')
         res.end()
     }
     // about page
-    else if (url === '/about') {
+    else if (req.url === '/about') {
         res.writeHead(200, { 'content-type': 'text/html' })
         res.write('<h1>about page</h1>')
         res.end()
