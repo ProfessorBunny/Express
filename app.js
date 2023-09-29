a7663aabcc68b73bd14131b39be3a7ba5b0cb00c
200~ProfessorBunny <nikunjraghav24@gmail.com>
2023-09-29 14:20:41 +0100

    wokring with basic express methods

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express = require('express')
 const app = express()
 
+app.get('/', (req, res) => {
+    res.status(200).end('Home Page')
+})
+app.get('/about', (req, res) => {
+    res.status(200).end('About Page')
+})
+
+app.all('*', (req, res) => {
+    res.status(404).end("Resouce not found")
+})
+
 app.listen(5000, () => {
     console.log("server is listening on port 5000")
 })
