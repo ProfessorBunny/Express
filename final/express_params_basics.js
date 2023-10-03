18884d8e5c525bb11ec176f649576716656e9655
200~ProfessorBunny <nikunjraghav24@gmail.com>
2023-10-03 12:00:17 +0100

    express request params

--- final/express_params_basics.js
+++ final/express_params_basics.js
@@ -5,12 +5,12 @@ const { products } = require('./data')
 app.get('/', (req, res) => {
     res.send('<h1>Home Page</h1><a href=" /api/products"> products </a>')
 })
-app.get('/api/products/:productID', (req, res) => {
-    const { productID } = req.params
-    const singleProduct = products.find((product) => (
-        product.id === Number(productID)
-    ))
-    res.json(singleProduct)
+app.get('/api/products', (req, res) => {
+    const newProducts = products.map((product) => {
+        const { id, name, image } = product
+        return { id, name, image }
+    })
+    res.json(newProducts)
 
 })
 
